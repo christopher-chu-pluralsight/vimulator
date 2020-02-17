f95d2ea5793b248199284844449d368e30a235ed
Christopher Chu <christopher-chu@pluralsight.com>
2020-02-16 22:44:09 -0500

    adding and deleting rows kind of works

--- components/App.js
+++ components/App.js
@@ -3,15 +3,23 @@ import React from 'react'
 import useVim from '../hooks/use-vim'
 
 const App = () => {
-  const [ mode, text ] = useVim()
+  const { mode, text, cursor } = useVim()
 
   return (
     <>
       <div>
-        Text: {text}
+        Text: {text.map((row, index) => (
+          <div key={index}>
+            {row.map((char, index) => (
+              <span key={index}>
+                {char}
+              </span>
+            ))}
+          </div>
+        ))}
       </div>
       <div>
-        Mode: {mode}
+        Mode: {mode} | {cursor.row}:{cursor.col}
       </div>
     </>
   )
--- hooks/use-vim.js
+++ hooks/use-vim.js
@@ -10,7 +10,9 @@ export default function useVim() {
   const { key, keyDate } = useKeyboard()
 
   const [ mode, setMode ] = useState(NORMAL)
-  const [ text, setText ] = useState('')
+  const [ text, setText ] = useState([[]])
+  const [ cursorRow, setCursorRow ] = useState(0)
+  const [ cursorCol, setCursorCol ] = useState(0)
 
   useEffect(() => {
     if (!IGNORED_KEYS.includes(key)) {
@@ -22,17 +24,22 @@ export default function useVim() {
           default:
             break
         }
-      }
-
+      } 
       else if (mode === INSERT) {
         switch(key) {
           case 'Escape':
             setMode(NORMAL)
             break
+          case 'Backspace':
+            deleteCharacter()
+            break
+          case 'Enter':
+            insertRow()
+            break
           case null:
             break
           default:
-            setText(text.concat(key))
+            addCharacter(key)
             break
         }
       }
@@ -41,6 +48,49 @@ export default function useVim() {
     return () => {}
   }, [ keyDate ]);
 
+  return { 
+    mode,
+    text,
+    cursor: { 
+      row: cursorRow + 1,
+      col: cursorCol + 1,
+    },
+  }
 
-  return [ mode, text ] 
+  function addCharacter(char) {
+    // TODO: not immutable
+    // TODO: figure out performance impact of index keys
+    text[cursorRow] = text[cursorRow].concat(char)
+
+    setCursorCol((prevCol) => prevCol + 1)
+    setText(text)
+  }
+
+  function deleteCharacter() {
+    if (cursorCol > 0) {
+      // TODO: not immutable
+      text[cursorRow] = text[cursorRow].slice(0, -1)
+      setCursorCol((prevCol) => prevCol - 1)
+      setText(text)
+    } else {
+      deleteRow()
+    }
+  }
+
+  function deleteRow() {
+    if (cursorRow > 0) {
+      const currentRow = text[cursorRow]
+      // TODO: carry over remaining contents to previous row
+      //    text[cursorRow - 1] = text[cursorRow - 1].concat(currentRow)
+      setCursorRow((prevRow) => prevRow - 1)
+      setCursorCol(0)
+      setText(text.filter((_value, index) => index !== cursorRow))
+    }
+  }
+
+  function insertRow() {
+    setText([ ...text, [] ])
+    setCursorCol(0)
+    setCursorRow((prevRow) => prevRow + 1)
+  }
 }
